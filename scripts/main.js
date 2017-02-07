51948d388ab40ca15d04ef60be43852e65d37046
Harry Westbrook <ig3nharry@gmail.com>
2017-02-07 17:07:48 +0000

    Removed Gridstack, Clock and todo.
    Removed Gridstack system as not appropriate for current dashboard idea.
    
    Implemented real time digital clock as well as a to do list with
    add-able and deleteable items.

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,71 @@ $('#panel-example').scotchPanel({
     distanceX: '30%', // Size fo the toggle
     enableEscapeKey: true // Clicking Esc will close the panel
 });
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('clock-widget').innerHTML =
+    h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
+
+function get_todos() {
+    var todos = new Array;
+    var todos_str = localStorage.getItem('todo');
+    if (todos_str !== null) {
+        todos = JSON.parse(todos_str);
+    }
+    return todos;
+}
+
+function add() {
+    var task = document.getElementById('task').value;
+
+    var todos = get_todos();
+    todos.push(task);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function remove() {
+    var id = this.getAttribute('id');
+    var todos = get_todos();
+    todos.splice(id, 1);
+    localStorage.setItem('todo', JSON.stringify(todos));
+
+    show();
+
+    return false;
+}
+
+function show() {
+    var todos = get_todos();
+
+    var html = '<ul>';
+    for(var i=0; i<todos.length; i++) {
+        html += '<li>' + todos[i] + '<button class="remove" id="' + i  + '">x</button></li>';
+    };
+    html += '</ul>';
+
+    document.getElementById('todos').innerHTML = html;
+
+    var buttons = document.getElementsByClassName('remove');
+    for (var i=0; i < buttons.length; i++) {
+        buttons[i].addEventListener('click', remove);
+    };
+}
+
+document.getElementById('add').addEventListener('click', add);
+show();
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var welcomebutton = document.getElementById('welcome-button');
 
 welcomebutton.addEventListener('click', changeUsername);
 
-function changeUsername(){
-  var nameValue = document.getElementById('nameinput').value;
-  var greeting = document.getElementById('greeting');
+function changeUsername() {
+    var nameValue = document.getElementById('nameinput').value;
+    var greeting = document.getElementById('greeting');
 
-  greeting.innerHTML = "Welcome " + nameValue;
+    greeting.innerHTML = "Welcome " + nameValue;
 }
