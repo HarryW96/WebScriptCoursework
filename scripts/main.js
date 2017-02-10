f4ea85dd53b1ca3f2b8e678baff3fb57d7fd2b23
Harry Westbrook <harrydavidwestbrook@gmail.com>
2017-02-10 09:49:25 +0000

    Added images,  added image carousel.
    Image carousel integrated wtih images added to /img folder.
    
    Began research and development into twitter widget api, bookmarked
    interesting source.

--- scripts/main.js
+++ scripts/main.js
@@ -75,3 +75,18 @@ function show() {
 
 document.getElementById('add').addEventListener('click', add);
 show();
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("carousel-img");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 8000); // Change image every 10 seconds
+}
