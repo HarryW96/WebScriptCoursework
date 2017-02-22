a40620f13c7ac87e023e450ff2bc7166a6934fc4
Harry Westbrook <harrydavidwestbrook@gmail.com>
2017-02-22 18:09:25 +0000

    Weather installed and testing

--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 var express = require('express')
+var Weather = require('weather.js')
 var app = express()
-var weather = require('weather-js')
+
 
 app.use(express.static('static'))
 
-
-weather.find({
-    search: 'Portsmouth, UK',
-    degreeType: 'C'
-},
-
-function(err, result) {
-    if (err) console.log(err);
-
-    console.log(JSON.stringify(result, null, 2));
+Weather.getCurrent("Kansas City", function(current) {
+  console.log(
+    ["currently:",current.temperature(),"and",current.conditions()].join(" ")
+  );
 });
 
 app.listen(8000, function() {
